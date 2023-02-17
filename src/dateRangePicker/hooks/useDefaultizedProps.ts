82ecdc4ddbbd47569bbe67dc601b024ad236af7f
Lenic Rayleigh <Lenic@live.cn>
2023-02-18 01:03:25 +0800

    重构 useDefaultizedProps 方法
    Signed-off-by: Lenic Rayleigh <Lenic@live.cn>

--- src/dateRangePicker/hooks/useDefaultizedProps.ts
+++ src/dateRangePicker/hooks/useDefaultizedProps.ts
@@ -1,37 +1,34 @@
+import type { BaseDateValidationProps, DefaultizedProps } from '@mui/x-date-pickers/internals';
+
 import type { BaseDateRangePickerProps } from '../types';
-import {
-  useDefaultDates,
-  useLocaleText,
-  useUtils,
-  DefaultizedProps,
-  parseNonNullablePickerDate,
-  BaseDateValidationProps,
-} from '@mui/x-date-pickers/internals';
+
+import { useMemo } from 'react';
 import { useThemeProps } from '@mui/material/styles';
+import { useDefaultDates, useLocaleText, useUtils, parseNonNullablePickerDate } from '@mui/x-date-pickers/internals';
 
 export function useDefaultizedProps<TInputDate, TDate, Props extends BaseDateRangePickerProps<TInputDate, TDate>>(
   props: Props,
   name: string
 ): DefaultizedProps<Props, keyof BaseDateValidationProps<TDate>, { inputFormat: string }> {
-  const utils = useUtils<TDate>();
-  const defaultDates = useDefaultDates<TDate>();
-
   // This is technically unsound if the type parameters appear in optional props.
   // Optional props can be filled by `useThemeProps` with types that don't match the type parameters.
-  const themeProps = useThemeProps({
-    props,
-    name,
-  });
+  const themeProps = useThemeProps({ props, name });
+  const { minDate: tmin, maxDate: tmax } = themeProps;
 
-  const localeText = useLocaleText();
-  return {
-    disableFuture: false,
-    disablePast: false,
-    inputFormat: utils.formats.keyboardDate,
-    ...themeProps,
-    endText: localeText.end,
-    startText: localeText.start,
-    minDate: parseNonNullablePickerDate(utils, themeProps.minDate, defaultDates.minDate),
-    maxDate: parseNonNullablePickerDate(utils, themeProps.maxDate, defaultDates.maxDate),
-  };
+  const utils = useUtils<TDate>();
+  const { end, start } = useLocaleText();
+  const { minDate, maxDate } = useDefaultDates<TDate>();
+  return useMemo(
+    () => ({
+      disableFuture: false,
+      disablePast: false,
+      inputFormat: utils.formats.keyboardDate,
+      ...themeProps,
+      endText: end,
+      startText: start,
+      minDate: parseNonNullablePickerDate(utils, tmin, minDate),
+      maxDate: parseNonNullablePickerDate(utils, tmax, maxDate),
+    }),
+    [end, maxDate, minDate, start, themeProps, tmax, tmin, utils]
+  );
 }
