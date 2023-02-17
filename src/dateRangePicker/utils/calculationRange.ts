333470da7f8b15cd0afd3e350fbbbd98f4797eac
Lenic Rayleigh <Lenic@live.cn>
2023-02-18 01:10:58 +0800

    重构 calculationRange 文件中的类型定义
    Signed-off-by: Lenic Rayleigh <Lenic@live.cn>

--- src/dateRangePicker/utils/calculationRange.ts
+++ src/dateRangePicker/utils/calculationRange.ts
@@ -2,34 +2,34 @@ import type { MuiPickersAdapter } from '@mui/x-date-pickers/internals';
 
 import type { DateRange, TFocusPosition } from '../types';
 
-export interface CalculateRangeChangeOptions<TDate> {
+export type TCalculateRangeChangeOptions<TDate> = {
   utils: MuiPickersAdapter<TDate>;
   highlightedRange: DateRange<TDate>;
   newDate: TDate;
   focusPosition: TFocusPosition;
-}
+};
 
-export interface ChangeRangeResult<TDate> {
+export type TChangeRangeResult<TDate> = {
   nextSelection: TFocusPosition;
   newRange: DateRange<TDate>;
-}
+};
 
-export function calculateChangeRange<TDate>(options: CalculateRangeChangeOptions<TDate>): ChangeRangeResult<TDate> {
+export function calculateChangeRange<TDate>(options: TCalculateRangeChangeOptions<TDate>): TChangeRangeResult<TDate> {
   const { utils, highlightedRange, newDate: selectedDate, focusPosition } = options;
   const [start, end] = highlightedRange;
 
   if (focusPosition === 'start') {
-    return Boolean(end) && utils.isAfter(selectedDate, end!)
+    return !!end && utils.isAfter(selectedDate, end!)
       ? { nextSelection: 'end', newRange: [selectedDate, null] }
       : { nextSelection: 'end', newRange: [selectedDate, end] };
   }
 
-  return Boolean(start) && utils.isBefore(selectedDate, start!)
+  return !!start && utils.isBefore(selectedDate, start!)
     ? { nextSelection: 'end', newRange: [selectedDate, null] }
     : { nextSelection: 'start', newRange: [start, selectedDate] };
 }
 
-export function calculateHighlightRange<TDate>(options: CalculateRangeChangeOptions<TDate>): DateRange<TDate> {
+export function calculateHighlightRange<TDate>(options: TCalculateRangeChangeOptions<TDate>): DateRange<TDate> {
   if (!options.newDate) {
     return [null, null];
   }
