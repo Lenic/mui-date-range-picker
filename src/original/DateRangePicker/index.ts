48b6cf6621009700f24f3c06a65e20d5f87fe74a
Lenic Rayleigh <Lenic@live.cn>
2023-02-17 22:38:04 +0800

    移除不再使用的文件
    Signed-off-by: Lenic Rayleigh <Lenic@live.cn>

--- src/original/DateRangePicker/index.ts
+++ src/original/DateRangePicker/index.ts
@@ -1,12 +1,1 @@
-export { getDateRangePickerInputUtilityClass, dateRangePickerInputClasses } from './dateRangePickerInputClasses';
-export type { DateRangePickerInputClasses, DateRangePickerInputClassKey } from './dateRangePickerInputClasses';
-export {
-  getDateRangePickerViewDesktopUtilityClass,
-  dateRangePickerViewDesktopClasses,
-} from './dateRangePickerViewDesktopClasses';
-export type {
-  DateRangePickerViewDesktopClasses,
-  DateRangePickerViewDesktopClassKey,
-} from './dateRangePickerViewDesktopClasses';
-
 export type { DateRange } from '../internal/models';
